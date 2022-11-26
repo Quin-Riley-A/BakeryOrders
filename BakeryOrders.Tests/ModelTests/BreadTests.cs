e4bfa646d480fc5e17ef4ba664097c49cc2ea746
Quin-Riley-A <quin.asselin1@gmail.com>
2022-11-26 16:58:33 -0500

    write second TDD for bread order

--- BakeryOrders.Tests/ModelTests/BreadTests.cs
+++ BakeryOrders.Tests/ModelTests/BreadTests.cs
@@ -10,8 +10,16 @@ namespace BakeryOrders.Models
     [TestMethod]
     public void ClassConstructor_CreatesInstanceOfBread_Bread()
     {
-      Bread newLoaves = new Bread();
+      Bread newLoaves = new Bread(0);
       Assert.AreEqual(typeof(Bread), newLoaves.GetType());
+    }
+    [TestMethod]
+    public void GetQuantity_ReturnsQuantity_Int()
+    {
+      int breadQty = 2;
+      Bread newLoaves = new Bread(breadQty);
+      int breadCount = newLoaves.BreadQuantity;
+      Assert.AreEqual(breadCount, breadQty);
     }
   }
 }
