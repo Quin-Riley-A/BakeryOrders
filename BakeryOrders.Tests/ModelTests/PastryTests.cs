e8db0deaec35d66a97b3f36df1692857955cb993
Quin-Riley-A <quin.asselin1@gmail.com>
2022-11-27 01:19:39 -0500

    adjust Assert.AreEqual() arguments for expected and actual.

--- BakeryOrders.Tests/ModelTests/PastryTests.cs
+++ BakeryOrders.Tests/ModelTests/PastryTests.cs
@@ -15,14 +15,21 @@ namespace BakeryOrders.Models
     {
       int pastryQty = 1;
       Pastry newDoughs = new Pastry(pastryQty);
-      Assert.AreEqual(newDoughs.PastryQuantity, 1);
+      Assert.AreEqual(1, newDoughs.PastryQuantity);
     }
     [TestMethod]
     public void GetPrice_AssignsPriceProperty_Int()
     {
       Pastry newDoughs = new Pastry(1);
       newDoughs.getPrice();
-      Assert.AreEqual(newDoughs.Price, 2);
+      Assert.AreEqual(2, newDoughs.Price);
+    }
+    [TestMethod]
+    public void GetPrice_AssignsPriceWithDiscount_Int()
+    {
+      Pastry newDoughs = new Pastry(7);
+      newDoughs.getPrice();
+      Assert.AreEqual(12, newDoughs.Price);
     }
   }
 }
