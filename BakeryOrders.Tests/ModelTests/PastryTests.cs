e885e6171bf60997796da2e54429a7f795fc1371
Quin-Riley-A <quin.asselin1@gmail.com>
2022-11-27 00:52:34 -0500

    adjust test 1 to account for constructor and describe test 2

--- BakeryOrders.Tests/ModelTests/PastryTests.cs
+++ BakeryOrders.Tests/ModelTests/PastryTests.cs
@@ -7,8 +7,15 @@ namespace BakeryOrders.Models
     [TestMethod]
     public void ClassConstructor_CreatesInstanceOfPastry_Pastry()
     {
-      Pastry newDoughs = new Pastry();
+      Pastry newDoughs = new Pastry(0);
       Assert.AreEqual(typeof(Pastry), newDoughs.GetType());
+    }
+    [TestMethod]
+    public void GetQuantity_ReturnsQuantity_Int()
+    {
+      int pastryQty = 1;
+      Pastry newDoughs = new Pastry(pastryQty);
+      Assert.AreEqual(newDoughs.PastryQuantity, 1);
     }
   }
 }
