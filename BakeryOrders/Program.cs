f6bbffbd8f744434983eb10b9a3f1aa75d65ef6a
Quin-Riley-A <quin.asselin1@gmail.com>
2022-11-27 02:28:39 -0500

    build welcome/landing page to abstract to separate function

---
+++ BakeryOrders/Program.cs
@@ -0,0 +1,45 @@
+using System;
+using BakeryOrders.Models;
+
+namespace BakeryOrders
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine(@"______ _                    _      ______       _                   ");
+      Console.WriteLine(@"| ___ (_)                  ( )     | ___ \     | |                  ");
+      Console.WriteLine(@"| |_/ /_  ___ _ __ _ __ ___|/ ___  | |_/ / __ _| | _____ _ __ _   _ ");
+      Console.WriteLine(@"|  __/| |/ _ \ '__| '__/ _ \ / __| | ___ \/ _` | |/ / _ \ '__| | | |");
+      Console.WriteLine(@"| |   | |  __/ |  | | |  __/ \__ \ | |_/ / (_| |   <  __/ |  | |_| |");
+      Console.WriteLine(@"\_|   |_|\___|_|  |_|  \___| |___/ \____/ \__,_|_|\_\___|_|   \__, |");
+      Console.WriteLine(@"                                                               __/ |");
+      Console.WriteLine(@"                                                              |___/ ");
+      Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+      Console.WriteLine
+      ("|Welcome to Pierre's Bakery!|");
+      Console.WriteLine
+      ("|Delicacies include:        |");
+      Console.WriteLine
+      ("| Breads by the loaf        |");
+      Console.WriteLine
+      ("|   One loaf is $5          |");
+      Console.WriteLine
+      ("|   Two loaves for $10. Get |");
+      Console.WriteLine
+      ("|   a third for free when   |");
+      Console.WriteLine
+      ("|   you buy two!            |");
+      Console.WriteLine
+      ("| Pastries by the morsel    |");
+      Console.WriteLine
+      ("|   One for $2,             |");
+      Console.WriteLine
+      ("|   Two for $4              |");
+      Console.WriteLine
+      ("|   Three for $5            |");
+      Console.WriteLine
+      ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
+  }
+}
